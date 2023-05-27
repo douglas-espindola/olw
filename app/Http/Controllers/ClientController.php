290f533e8a7be226215b98e4209eedd131004e04
Douglas Espindola <douglas.spindola15@gmail.com>
2023-05-27 20:14:13 -0300

    removido variaveis

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,37 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Client;
-use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StoreClientRequest;
 use App\Http\Requests\UpdateClientRequest;
+use App\Models\Client;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class ClientController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $clients = Client::with('user', 'address', 'sales')->paginate(10);
-
-        return $clients;
-
-        return view('clients.index', compact('clients'));
+        return view('clients.index');
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('clients.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreClientRequest  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(StoreClientRequest $request)
     {
@@ -40,7 +43,7 @@ class ClientController extends Controller
             $user = User::create([
                 'email' => $request->get('email'),
                 'name' => $request->get('name'),
-                'password' => Hash::make('12345678')
+                'password' => Hash::make('123456')
             ]);
 
             $user->client()->create([
@@ -52,30 +55,29 @@ class ClientController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Client $client)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
      */
     public function edit(Client $client)
     {
-        //
+        return view('clients.edit', compact('client'));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateClientRequest  $request
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
      */
     public function update(UpdateClientRequest $request, Client $client)
     {
         DB::transaction(function () use ($request, $client) {
-            $client->user::update([
+            $client->user->update([
                 'email' => $request->get('email'),
-                'name' => $request->get('name'),
+                'name' => $request->get('name')
             ]);
 
             $client->update([
@@ -88,6 +90,9 @@ class ClientController extends Controller
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Client $client)
     {
