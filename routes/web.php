690b53f8e2d2147357831aca2400ecf852530aa8
Douglas Espindola <douglas.spindola15@gmail.com>
2023-05-28 22:05:11 -0300

    adicionado script para dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use OpenAI\Laravel\Facades\OpenAI;
+use App\Http\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaleController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
-use App\Models\SalesCommission;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +21,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', Dashboard::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -31,18 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('/clients', ClientController::class);
-
-    Route::get('/chat', function () {
-        $fields = implode(',', SalesCommission::getColumns());
-        $question = 'gere um gráfico das vendas por empresa no eixo y ao logo dos últimos 5 anos';
-        $config =  OpenAI::completions()->create([
-            'model' => 'text-davinci-003',
-            'prompt' => "Considerando a lista de campos($fields), gere uma configuração json do Vega-lite v5 (sem campo de dados e com descrição) que atenda o seguinte pedido $question .Resposta:",
-            'max_tokens' => 1500
-        ])->choices[0]->text;
-
-        dd($config);
-    });
+    Route::get('/sales', [SaleController::class, 'index']);
 });
 
 
